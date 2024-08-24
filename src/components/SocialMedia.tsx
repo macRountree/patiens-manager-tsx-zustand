5c1c48f7e16830ba52a1b27842b17e2ca0090d8c
MacRountree <macoy1492@hotmail.com>
2024-08-23 20:20:07 -0700

    feat: Update social media links styling in SocialMedia component

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -10,28 +10,28 @@ export const SocialMedia = () => {
         <a
           href="https://portfoliomacrountree.netlify.app/"
           target="_blank"
-          className="hover:cursor-pointer hover:scale-110 hover:shadow-sm transition-transform duration-300 hover:text-white "
+          className="hover:cursor-pointer hover:scale-110 hover:shadow-sm transition-transform duration-300 text-white "
         >
           <FaLaptopCode className="h-12 w-12" />
         </a>
         <a
           href="https://github.com/macRountree/Kcal_counter"
           target="_blank"
-          className="hover:cursor-pointer hover:scale-110 hover:shadow-sm transition-transform duration-300 hover:text-white "
+          className="hover:cursor-pointer hover:scale-110 hover:shadow-sm transition-transform duration-300 text-white "
         >
           <FaGithub className="h-12 w-12" />
         </a>
         <a
           href="https://www.linkedin.com/in/macrountree/"
           target="_blank"
-          className="hover:scale-110 hover:shadow-sm transition-transform duration-300 hover:text-white "
+          className="hover:scale-110 hover:shadow-sm transition-transform duration-300 text-white "
         >
           <FaLinkedin className="h-12 w-12" />
         </a>
         <a
           href="https://wa.me/526622277834"
           target="_blank"
-          className="hover:scale-110 hover:shadow-sm transition-transform duration-300 hover:text-white "
+          className="hover:scale-110 hover:shadow-sm transition-transform duration-300 text-white "
         >
           <FaWhatsapp className="h-12 w-12" />
         </a>
